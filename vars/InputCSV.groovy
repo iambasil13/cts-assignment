fada51e63ad844845704087dbe4c0be1bfb92221
iambasil13 <47686156+iambasil13@users.noreply.github.com>
2021-06-21 15:23:19 +0530

    Update InputCSV.groovy

--- vars/InputCSV.groovy
+++ vars/InputCSV.groovy
@@ -1,7 +1,7 @@
 @Grab('com.xlson.groovycsv:groovycsv:1.3')
 import static com.xlson.groovycsv.CsvParser.parseCsv
  
-fh = new File('$WORKSPACE\\Input_CaseStudy.csv')
+fh = new File('$WORKSPACE\Input_CaseStudy.csv')
 def csv_content = fh.getText('utf-8')
  
 def data_iterator = parseCsv(csv_content, separator: ';', readFirstLine: true)
